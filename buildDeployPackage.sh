0027a5ab75ea7794c05d895cd74ebf32cf3822d0
Matthew Simpson <matthew.simpson@readycomputing.com>
2019-12-10 13:00:06 -0800

    commit

--- buildDeployPackage.sh
+++ buildDeployPackage.sh
@@ -4,14 +4,13 @@ set timeout 60
 
 set environment   			[lindex $argv 0]
 set buildNamespace			[lindex $argv 1]
-set deployFilePath			[lindex $argv 2]
-set deployFileName			[lindex $argv 3]
-set gitSourceBranch			[lindex $argv 4]
-set gitIntBranch			[lindex $argv 5]
+set deployFileName			[lindex $argv 2]
+set gitSourceBranch			[lindex $argv 3]
+set gitIntBranch			[lindex $argv 4]
 
 spawn csession $environment -U $buildNamespace 
 
-expect "$buildNamespace>" { send "Write ##class(User.SourceControl.Git.Utils).BuildDeployment(\"$deployFilePath\",\"$deployFileName\",\"$gitIntBranch\",\"$gitSourceBranch\")\r" } timeout { exit 1 }
+expect "$buildNamespace>" { send "Write ##class(User.SourceControl.Git.Utils).BuildDeployment(\"$deployFileName\",\"$gitIntBranch\",\"$gitSourceBranch\")\r" } timeout { exit 1 }
 
 expect "$buildNamespace>"  { send "H\r"}
 
